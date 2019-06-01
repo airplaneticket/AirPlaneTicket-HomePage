9b777f8cb6ed340fe9a4aeb84c7f9a4f657a1462
17520407 <17520407@gm.uit.edu.vn>
2019-06-01 10:26:07 +0700

    Update layout profike

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,6 +1,7 @@
 module.exports = routes => {
     routes.use('/login', require('./login/login.routes'));
     routes.use('/register', require('./register/register.routes'));
+    routes.use('/', require('./profile/index'));
     routes.use('/', require('./booking/index'));
     routes.use('/', require('./homepage/index'));
 }
