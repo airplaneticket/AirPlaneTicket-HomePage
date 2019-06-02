db0c8a513665356ddf6dab9111c7e2b765e9bd6c
17520407 <17520407@gm.uit.edu.vn>
2019-06-02 08:38:50 +0700

    update notification and comment code

--- src/app/public/homepage/js/custom.auth.js
+++ src/app/public/homepage/js/custom.auth.js
@@ -2,6 +2,9 @@ jQuery(document).ready(function($){
     $('#item1').on('click', function () {
         $('#item1 .flight-detail-info').toggle();
     });
+
+    //Chỗ này m kiểm tra khi mà gửi mail cho nó thì hiện dòng này lên callback ắ
+    $('#modelRegisterSuccess').modal('show')
 })
 
 
