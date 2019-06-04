842d68fac15c206c7ed86c99ded3aee8e6f4c50d
Phong2421999 <17520897@gm.uit.edu.vn>
2019-06-04 15:51:47 +0700

    add datetime services

--- src/app/controller/booking.controller.js
+++ src/app/controller/booking.controller.js
@@ -1,18 +1,24 @@
+const flightModel = require('../../models/flight.model');
+const dateTimeServices = require('../../services/date-time.services');
+const moment = require('moment');
+
 module.exports.postSearchBooking = async(req, res) => {
     let inputData = {
         from: req.body.bookingFrom,
         destination: req.body.bookingDestination,
         departTime: req.body.bookingDepartTime,
         returnTime: req.body.bookingReturnTime,
-        numberOfCustomer: req.body.bookingNumberOfCustomer,
         seatClass: req.body.bookingSeatClass,
     }
-    for (property in inputData) {
-        if (inputData[property] === '') {
-            delete inputData[property];
+    console.log(moment(dateTimeServices.getCurrentDate()).format('DD MM YYYY'));
+    if (req.body.bookingFlightType === 'oneWayTicket') {
+        for (data in inputData) {
+            if (inputData[data] == '') {
+                delete inputData[data];
+            }
         }
+        let flight = await flightModel.find({ inputData, departTime: { $gte: dateTimeServices.getCurrentDate() } });
     }
-    console.log(inputData);
     res.render('homepage/booking/booking.ejs', {
         searchData: inputData
     });
